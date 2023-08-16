deab32c6d12032471c58c3e64e07752c1101032d
Piyushshind <piyushshinde.199@gmail.com>
2023-08-16 21:24:03 +0530

    C practice

---
+++ prac.c
@@ -0,0 +1,12 @@
+# include<stdio.h>
+#include<math.h>
+
+int main(){
+    int a,b;
+    a=5;
+    // scanf("%d", &age);
+    int c= 8;
+    int ans = pow(c,a);
+    printf("%d", ans);
+    return 0 ;
+}  
