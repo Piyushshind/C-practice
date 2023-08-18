69132476440022c889fa84a31e535cb796af8694
Piyushshind <piyushshinde.199@gmail.com>
2023-08-18 20:02:41 +0530

    prac

--- prac.c
+++ prac.c
@@ -2,12 +2,12 @@
 #include<math.h>
 
 int main(){
-    // int a,b;
-    // a=5;
-    // scanf("%d", &age);
-    // int c= 8;
-    // int ans = pow(c,a);
-    // printf("%d", ans);
+    int a,b;
+    a=5;
+    scanf("%d", &age);
+    int c= 8;
+    int ans = pow(c,a);
+    printf("%d", ans);
     for(char i='a';i<='z';i++){
         printf("%c",i);
     }
