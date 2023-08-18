b2ee2fd7216076419c9d771637e304e549c003c1
Piyushshind <piyushshinde.199@gmail.com>
2023-08-18 20:06:45 +0530

    cheack the character is uppercase or not

--- CheakUpperCase.c
+++ CheakUpperCase.c
@@ -4,14 +4,14 @@ int main(){
     char cha;
     printf("enter a charectrt :- %c");
     scanf("%d", & cha);
-    // scanf("%d", &age); 
+    scanf("%d", &age); 
 
-    // if(cha >= 'A' && cha <= 'Z'){
-    //     printf("It is a Uppercase letter \n");
-    // } else if(cha >='a' && cha <='z'){
-    //     printf("It is Lower case letter");
-    // }else{
-    //     printf("Enter a valid character");
-    // }
+    if(cha >= 'A' && cha <= 'Z'){
+        printf("It is a Uppercase letter \n");
+    } else if(cha >='a' && cha <='z'){
+        printf("It is Lower case letter");
+    }else{
+        printf("Enter a valid character");
+    }
     return 0;
 }
