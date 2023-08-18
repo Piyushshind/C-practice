037731d48a903f52bb1de32a1f433929389ce3a7
Piyushshind <piyushshinde.199@gmail.com>
2023-08-18 20:01:29 +0530

    c practice

---
+++ CheakUpperCase.c
@@ -0,0 +1,17 @@
+# include<stdio.h>
+
+int main(){
+    char cha;
+    printf("enter a charectrt :- %c");
+    scanf("%d", & cha);
+    // scanf("%d", &age); 
+
+    // if(cha >= 'A' && cha <= 'Z'){
+    //     printf("It is a Uppercase letter \n");
+    // } else if(cha >='a' && cha <='z'){
+    //     printf("It is Lower case letter");
+    // }else{
+    //     printf("Enter a valid character");
+    // }
+    return 0;
+}
--- prac.c
+++ prac.c
@@ -2,11 +2,14 @@
 #include<math.h>
 
 int main(){
-    int a,b;
-    a=5;
+    // int a,b;
+    // a=5;
     // scanf("%d", &age);
-    int c= 8;
-    int ans = pow(c,a);
-    printf("%d", ans);
+    // int c= 8;
+    // int ans = pow(c,a);
+    // printf("%d", ans);
+    for(char i='a';i<='z';i++){
+        printf("%c",i);
+    }
     return 0 ;
 }  
