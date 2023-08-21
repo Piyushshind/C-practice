fa8ffb8b8813f0a7bc682ee6258de176ac141ea4
Piyushshind <piyushshinde.199@gmail.com>
2023-08-21 21:50:51 +0530

    call by value

--- Pointer/pointerByRefrance.c
+++ Pointer/pointerByRefrance.c
@@ -1,14 +1,28 @@
-# include <stdio.h>
-  
-  void square(int n);
+#include <stdio.h>
 
-  int main(){
-      int num = 7;
-      square(num);
-   return 0;
-  };
+void square(int n);
+void _square(int *n);
 
-  void square(int n){
-       n = n * n;
-       printf(" square of n is %d \n" , n);
-  }
+int main()
+{
+    int num = 7;
+    square(num);
+    printf("no. is :- %d \n", num);
+    printf("address of num  is :- %d \n", &num);
+    _square(&num);
+    printf("no. is :- %d \n", num);
+
+    return 0;
+};
+
+void square(int n)
+{
+    n = n * n;
+    printf(" square of n is %d \n", n);
+}
+
+void _square(int *n)
+{
+    *n = (*n) * (*n);
+    printf(" square of pointer *n is %d \n", *n);
+}
