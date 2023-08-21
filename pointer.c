bb7443fb8fc68ffab1cce0c7cf57bd3fe455787a
Piyushshind <piyushshinde.199@gmail.com>
2023-08-21 21:13:39 +0530

    pointer

--- pointer.c
+++ pointer.c
@@ -5,6 +5,11 @@ int main(){
 int *ptr = &x;
 int **pptr = &ptr;
 
-printf( "%d", ptr);
+printf( "%d\n", ptr);
+printf( "%d \n", *ptr);
+printf( "%d \n", &ptr);
+printf( "%d \n", **pptr);
+printf( "%d \n", &pptr);
+
 return 0;
 }
