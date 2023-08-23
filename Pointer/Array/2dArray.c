dca9c232fb016ad320f6c813cf643686332561a1
Piyushshind <piyushshinde.199@gmail.com>
2023-08-23 21:43:15 +0530

    2D array print table

--- Pointer/Array/2dArray.c
+++ Pointer/Array/2dArray.c
@@ -1,18 +1,29 @@
-# include <stdio.h>
+#include <stdio.h>
+// print table od 2 & 3 in 2D array ;
+int addTable(int arr[][10], int n, int m, int num);
 
- int addTable(int arr[][10],int n , int m , int num);
-
-int main(){
+int main()
+{
     int tables[2][10];
-     addTable(tables,10,0,2);
-     addTable(tables,10,1,3);
+    addTable(tables, 10, 0, 2);
+    addTable(tables, 10, 1, 3);
+
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d \t", tables[0][i]);
+    }
+    printf("\n");
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d \t", tables[1][i]);
+    }
     return 0;
 }
 
-int addTable(int arr[][10],int n , int m , int num){
+int addTable(int arr[][10], int n, int m, int num)
+{
     for (int i = 0; i < n; i++)
     {
-          arr[m][i] = (i+1) * num;
+        arr[m][i] = (i + 1) * num;
     }
-    
 }
