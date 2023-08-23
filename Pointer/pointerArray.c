70c1e5a9d9bfa2541d577a704f34e26b3aef3927
Piyushshind <piyushshinde.199@gmail.com>
2023-08-23 09:38:56 +0530

    pointer array

--- Pointer/pointerArray.c
+++ Pointer/pointerArray.c
@@ -1,2 +1,12 @@
 #include <stdio.h>
 
+int main(){ // Every array is a pointer ;
+    int age[] = {23,45,32};
+    int *_age = &age[1];
+    printf("address of 2nd is %u" , _age);
+     _age++;
+    printf(" \n address of 2nd is %u" , _age);
+    _age--;
+    printf(" \n address of 2nd is %u" , _age);
+    return 0;
+}
