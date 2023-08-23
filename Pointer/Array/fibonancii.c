f0fa3d7227d201b50fd110ada0ba369fab4dd9bd
Piyushshind <piyushshinde.199@gmail.com>
2023-08-23 20:47:36 +0530

    fibonancci

--- Pointer/Array/fibonancii.c
+++ Pointer/Array/fibonancii.c
@@ -1,34 +1,20 @@
-# include <stdio.h>
+#include <stdio.h>
 
-void printFibo(int arr[], int n);
-int fibo(int n);
-void printVal(int arr[], int n);
+void printFibo(int n);
 
-int main(){
-   int arr1[]= {};
-    printFibo(arr1,8);
-     printVal(arr1,8);
-    return 0;
-}
-
-void printFibo(int arr[],int n){
-    for (int i = 2; i <n; i++)
-    {
-        arr[i]= fibo(i);
-    }
-    
-};
-
-int fibo(int n){
-    if( n==1) return 1;
-    if(n==0) return 0;
-      return fibo(n-1) + fibo(n-2);
-}
-
-void printVal(int arr[], int n)
+int main()
 {
-    for (int i = 0; i < n; i++)
+    int n;
+    printf("Enter a number upto which you want to print fibonnaci series :-  %d", n);
+    scanf("%d", &n);
+    int arr[n];
+    arr[0] = 0;
+    arr[1] = 1;
+
+    for (int i = 2; i < n; i++)
     {
-        printf(" %d", arr[i]);
+        arr[i] = arr[i - 1] + arr[i - 2];
+        printf("%d \t", arr[i]);
     }
+    return 0;
 }
