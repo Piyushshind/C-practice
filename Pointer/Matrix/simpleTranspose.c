f898e7b97d48995e2004449421f11063d5aae9f0
Piyushshind <piyushshinde.199@gmail.com>
2023-09-17 20:33:05 +0530

    c

---
+++ Pointer/Matrix/simpleTranspose.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int row ,col ;
+    scanf("Enter the value of row :- %d " , & row);
+    scanf("Enter the value of colums :- %d " , & col);
+    int arr[row][col];
+
+    return 0;
+}
