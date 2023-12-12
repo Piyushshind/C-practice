8b9d0915c7f5a2cedf7d04a53ea54a4dd4d5381d
Piyushshind <piyushshinde.199@gmail.com>
2023-12-12 18:06:24 +0530

    c

--- prac.c
+++ prac.c
@@ -1,15 +1,11 @@
-# include<stdio.h>
-#include<math.h>
-
-int main(){
-    int a,b;
-    a=5;
-    //scanf("%d", & age);
-    int c= 8;
-    int ans = pow(c,a);
-    printf("%d", ans);
-    for(char i='a';i<='z';i++){
-    printf("%c",i);
-    }
-    return 0 ;
-}  
+#include <stdio.h> 
+int func() 
+{ 
+      int a = 10; 
+     return a, a * 2; 
+} 
+int main() 
+{ 
+     printf("%d", func()); 
+     return 0; 
+}
